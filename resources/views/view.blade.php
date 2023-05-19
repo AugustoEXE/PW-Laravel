5e07136d39782f447451c06e809c03d66bd0dac1
Augusto Cristofoli <106482872+AugustoEXE@users.noreply.github.com>
2023-05-19 11:14:19 -0300

    aaaaaaa

---
+++ resources/views/view.blade.php
@@ -0,0 +1,7 @@
+@extends('includes.base')
+
+@section('title', 'Título joia')
+
+@section('content')
+<marquee behavior="scroll" SCROLLAMOUNT=10 direction="" class="" >TESTE VIEW BLADE LARAVEL 😱😱 {{$param}}</marquee>
+@endsection
