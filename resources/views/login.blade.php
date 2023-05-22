0d7dd50f5977f417d7f6ac57b7a808dfbe45b9dc
Augusto Rodrigues <augusto.270605@bento.local>
2023-05-22 08:51:56 -0300

    tailwind adicionado

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Login</h1>
-        <form action=""></form>
-    </div>
-</body>
-</html>
+
+@extends('includes.base')
+
+@section('title', 'Título joia')
+
+@section('content')
+<div class="container">
+    <h1 class="text-6xl font-bold bg">
+        Hello world!
+      </h1>
+    <form action=""></form>
+</div>
+@endsection
